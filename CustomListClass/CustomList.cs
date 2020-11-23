6bc1be9afb3881810f6a3bffe44d2f881ed6b7ba
JoeYashinsky <joey.yashinsky@gmail.com>
2020-11-23 13:42:05 -0500

    Wrote opening Add test titled 'Add_Positive_Integer_CountOf1'

---
+++ CustomListClass/CustomList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class CustomList
+    {
+
+
+
+    }
+}
+
+//public class CustomList<T>
+//{
+//    T[] _items;
+//    public int Count;
+//    public int Capacity;
+
+
+//    public CustomList()
+//    {
+//        Count = 0;
+//        Capacity = 4;
+//        _items = new T[Capacity];
+//    }
+
+
+//    public void Add(T valueToAdd)
+//    {
+//        _items[Count] = valueToAdd;
+//        Count++;
+
+//    }
+//}
--- CustomListUnitTests/UnitTest1.cs
+++ CustomListUnitTests/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using 
+using CustomListClass;
 
 namespace CustomListUnitTests
 {
@@ -8,8 +8,78 @@ namespace CustomListUnitTests
     public class CustomListUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_PositiveInteger_CountOf1()
         {
+            //Arrange
+            CustomList<int> numberList = new CustomList<int>();
+            int firstNumber = 3;
+            int expected = 1;
+            int actual;
+
+            //Act
+            numberList.Add(firstNumber);
+            actual = numberList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
         }
     }
 }
+
+//public void Add_PostiveInteger_CountOfOne()
+//{
+//    //arrange
+//    CustomList<int> myList = new CustomList<int>();
+//    int value1 = 5;
+//    int expected = 1;
+//    int actual;
+//    //act
+//    myList.Add(value1);
+//    actual = myList.Count;
+//    //assert
+//    Assert.AreEqual(expected, actual);
+//}
+
+//[TestMethod]
+//public void Add_PostiveIntegers_CountOfFour()
+//{
+//    //arrange
+//    CustomList<int> myList = new CustomList<int>();
+//    int value1 = 5;
+//    int value2 = 5;
+//    int value3 = 5;
+//    int value4 = 5;
+//    int expected = 4;
+//    int actual;
+//    //act
+//    myList.Add(value1);
+//    myList.Add(value2);
+//    myList.Add(value3);
+//    myList.Add(value4);
+//    actual = myList.Count;
+//    //assert
+//    Assert.AreEqual(expected, actual);
+//}
+
+//[TestMethod]
+//public void Add_PostiveIntegers_CapacityOfFour()
+//{
+//    //arrange
+//    CustomList<int> myList = new CustomList<int>();
+//    int value1 = 5;
+//    int value2 = 5;
+//    int value3 = 5;
+//    int value4 = 5;
+//    int expected = 4;
+//    int actual;
+//    //act
+//    myList.Add(value1);
+//    myList.Add(value2);
+//    myList.Add(value3);
+//    myList.Add(value4);
+//    actual = myList.Capacity;
+//    //assert
+//    Assert.AreEqual(expected, actual);
+//}
