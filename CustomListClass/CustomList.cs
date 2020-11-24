c56c942f4e30402420b8b6b4e3a2116f0de56d73
JoeYashinsky <joey.yashinsky@gmail.com>
2020-11-24 10:41:41 -0500

    Added read-only Count property to CustomList class. Added a Capacity property to CustomList class.

--- CustomListClass/CustomList.cs
+++ CustomListClass/CustomList.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace CustomListClass
 {
     public class CustomList<T>
-    {
-        T[] _items;
-        public int Count;
-        public int Capacity;
+    {                           
+        private T[] _items;
+        private int Count;
+        private int Capacity;
         
         public T this[int index]
         {get
@@ -19,7 +19,22 @@ namespace CustomListClass
             }
 
         }
+
+        public int Count                //As a developer, I want a 'read-only' Count property implemented on the custom-built list class
+        {
+            get
+            {
+                return Count;
+            }
+        }
         
+        public int Capacity            //As a developer, I want a Capacity property implemented on the custom-built list class
+        {
+            get
+            {
+                return Capacity;
+            }
+        }
 
         public CustomList()
         {
@@ -28,11 +43,13 @@ namespace CustomListClass
             _items = new T[Capacity];
         }
 
+
         public void Add(T valueToAdd)
         {
             _items[Count] = valueToAdd;
             Count++;
         }
+
         public void Remove(T valueToAdd)
         {
             _items[Count] = valueToAdd;
